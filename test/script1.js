30b45738c3ed968a95e99ae5118257514cb73b83
praval2510 <praval2510@gmail.com>
2024-11-08 10:16:26 +0530

    Second Commit

--- test/script1.js
+++ test/script1.js
@@ -1,9 +1,9 @@
 function handleSubmit(event) {
-    event.preventDefault(); // Prevent form submission
-    // Get values
+    event.preventDefault(); 
+    
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
     
-    // Simple redirect to next page (replace 'nextpage.html' with your URL)
+  
     window.location.href = 'nextpage.html';
 }
